9a20cf7775faee585bd76b14516f1dc66e340706
Mohammad Noor <mohammad.noor@mail.mcgill.ca>
2022-08-25 16:28:20 +0000

    Added Stock Order history button in Nav Bar. Able to go to Stock Order History Page.

--- PortfolioManagementAppFrontend/src/app/app-routing.module.ts
+++ PortfolioManagementAppFrontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderHistoryComponent } from './order-history/order-history.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 
 const routes: Routes = [
-  { path: 'portfolio', component: PortfolioComponent }
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'orderHistory', component: OrderHistoryComponent }
 ];
 
 @NgModule({
